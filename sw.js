a87d23d96fa696ff7c804570ff505210c40d29c1
uniquesakshi1 <fationplanet@gmail.com>
2025-09-07 12:27:52 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,6 +11,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
